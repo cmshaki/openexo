6b8578ddda8eddf649fdbc8408a09c8aa72af96b
Cris Shaki <cmshaki@gmail.com>
2019-10-05 13:42:37 +0300

    Finished working on the menu completely

--- src/assets/shared/js/scroll.js
+++ src/assets/shared/js/scroll.js
@@ -1,4 +1,5 @@
 const main = document.querySelector("main");
+const menu = document.querySelector(".nav-menu");
 const menuLines = document.querySelectorAll(".nav-menu .line");
 const menuText = document.querySelector(".nav-menu .text");
 
@@ -6,29 +7,51 @@ let top = main.getBoundingClientRect().top;
 let windowHeight = window.innerHeight / 2;
 
 if (top < windowHeight) {
-  menuText.setAttribute("style", "color: #000");
-  menuLines.forEach(item => {
-    item.setAttribute("style", "background: #000");
-  });
+  if (
+    !menuText.style.color ||
+    menuText.style.color == "#ffffff" ||
+    menuText.style.color == "rgb(255, 255, 255)"
+  ) {
+    menuText.setAttribute("style", "color: #000");
+    menuLines.forEach(item => {
+      item.setAttribute("style", "background: #000");
+    });
+  }
 } else {
-  menuText.setAttribute("style", "color: #fff");
-  menuLines.forEach(item => {
-    item.setAttribute("style", "background: #fff");
-  });
+  if (
+    menuText.style.color == "#000000" ||
+    menuText.style.color == "rgb(0, 0, 0)"
+  ) {
+    menuText.setAttribute("style", "color: #ffffff");
+    menuLines.forEach(item => {
+      item.setAttribute("style", "background: #ffffff");
+    });
+  }
 }
 
 document.addEventListener("scroll", () => {
   top = main.getBoundingClientRect().top;
   windowHeight = window.innerHeight / 2;
   if (top < windowHeight) {
-    menuText.setAttribute("style", "color: #000");
-    menuLines.forEach(item => {
-      item.setAttribute("style", "background: #000");
-    });
+    if (
+      !menuText.style.color ||
+      menuText.style.color == "#ffffff" ||
+      menuText.style.color == "rgb(255, 255, 255)"
+    ) {
+      menuText.setAttribute("style", "color: #000000");
+      menuLines.forEach(item => {
+        item.setAttribute("style", "background: #000000");
+      });
+    }
   } else {
-    menuText.setAttribute("style", "color: #fff");
-    menuLines.forEach(item => {
-      item.setAttribute("style", "background: #fff");
-    });
+    if (
+      menuText.style.color == "#000000" ||
+      menuText.style.color == "rgb(0, 0, 0)"
+    ) {
+      menuText.setAttribute("style", "color: #ffffff");
+      menuLines.forEach(item => {
+        item.setAttribute("style", "background: #ffffff");
+      });
+    }
   }
 });
